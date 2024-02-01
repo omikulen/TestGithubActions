8c507bbae0c82712f3a9d80436a06027cd914bbe
omikulen <omikulen@users.noreply.github.com>
2024-02-01 17:10:13 +0000

    Automatic formatting

--- python_testing_files/test_1.py
+++ python_testing_files/test_1.py
@@ -1,12 +1,10 @@
 from pytest import raises
 
 
-def    add(a, b):
-    return  a   +b
+def add(a, b):
+    return a + b
 
 
-def test_add(mocker  ):
-    assert add(1, 2
-               ) == 3
-    assert raises(
-        TypeError, add, 1 , "2")
+def test_add(mocker):
+    assert add(1, 2) == 3
+    assert raises(TypeError, add, 1, "2")
